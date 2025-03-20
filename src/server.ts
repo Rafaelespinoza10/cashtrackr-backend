060a4750ede48781ed98b48e0d49dec2d9ab4ec8
Rafael_Espinoza~ <rafael.moreno.espinoza10@gmail.com>
2025-03-19 20:28:17 -0600

    fix server.js

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,15 @@ import colors from 'colors'
 import morgan from 'morgan'
 import budgetRouter from './routes/routes';
 import authRouter from './routes/auth.routes';
+import cors from 'cors';
 
 const app = express()
+
+app.use(cors({
+    origin: `${process.env.CLIENT_URL}`,  // o bien un array con tus orígenes permitidos
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // si manejas cookies, tokens, etc.
+  }));
 
 app.use(morgan('dev'))
 
